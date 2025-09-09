36b368e7b6ae8aa35126836624a7805deb5a9578
shivamaurya768 <mauryashivam2233@gmail.com>
2025-09-09 23:47:59 +0530

    dynmic quiz for iot

---
+++ script.js
@@ -0,0 +1,557 @@
+let q = [
+            ["Q-->1 what does IOT stand for",
+                {
+                    "internet of Technology": false,
+                    "interfece of things": false,
+                    "internet of things": true,
+                    "None": false
+                }
+            ],
+            ["Q-->2 which protocal is commonly used in IOT device for lightweight commnunication ?",
+                {
+                    "HTTP": false,
+                    "MQTT": true,
+                    "FTP": false,
+                    "TCP": false
+                }
+            ],
+            ["Q-->3 What is a major challenge in IOT ",
+                {
+                    "data processing": false,
+                    "scalability": false,
+                    "cloud storage": false,
+                    "security": true
+                }
+            ],
+            ["Q-->4 Which wireless standard is used in iot for low power and long-range ?",
+                {
+                    "LTE": false,
+                    "Ethernet": false,
+                    "Zigbee": true,
+                    "USB": false
+                }
+            ],
+            ["Q-->5 What does CoAP stand for ?",
+                {
+                    "constained application protocal": true,
+                    "control application protocal": false,
+                    "cloud access protocal": false,
+                    "common access point": false
+                }
+            ],
+            ["Q-->6 The term of IOT was used by ",
+                {
+                    "IB": false,
+                    "kevin shoff": false,
+                    "guido van rossum": false,
+                    "kevin Ashton": true
+                }
+            ],
+            ["Q-->7 How many number of element in the open IOT architecture ?",
+                {
+                    "3 Element": false,
+                    "5 Element": false,
+                    "7 Element": true,
+                    "6 Element": false
+                }
+            ],
+            ["Q-->8 Which of the following is not an appcation of IOT ",
+                {
+                    "Arduino": true,
+                    "smart Grid": false,
+                    "smart city": false,
+                    "none": false
+                }
+            ],
+            ["Q-->9 the full form of LoRa is  ",
+                {
+                    "level range radio ": false,
+                    "long range radio": true,
+                    "long radio range": false,
+                    "None of these": false
+                }
+            ],
+            ["Q-->10 The iot architecture contains how many layers ",
+                {
+                    "10": false,
+                    "9": false,
+                    "8": false,
+                    "7": true
+                }
+            ],
+            ["Q-->11 Bluethooth is operater on which type of network ",
+                {
+                    "WPAN": true,
+                    "LAN": false,
+                    "WAN": false,
+                    "GAN": false
+                }
+            ],
+            ["Q-->12 what is the full form Wi-Fi ? ",
+                {
+                    "wired fidelity": false,
+                    "wireless fidelity": true,
+                    "wireless frequncy": false,
+                    "None of these": false
+                }
+            ],
+            ["Q-->13 RFID stands for ?",
+                {
+                    "Radio frequency image": false,
+                    "Radio frequency identification": true,
+                    "Read frequency identification": false,
+                    "None of these": false
+                }
+            ],
+            ["Q-->14 Which of the following is not involved in working of IOT ?",
+                {
+                    "server": true,
+                    "RFID": false,
+                    "sensor": false,
+                    "All of these": false
+                }
+            ],
+            ["Q-->15 Internet of things has... ?",
+                {
+                    "Greater transpareny": false,
+                    "control": false,
+                    "performance": false,
+                    "all of these": true
+                }
+            ],
+            ["Q-->16 the device connected to the internet of things have to communication autometically not via human . What is this called ?",
+                {
+                    "B2b": false,
+                    "M2M": true,
+                    "intercloud": false,
+                    "skynet": false
+                }
+            ],
+            ["Q--> 17 iot promotes the creation of iot terminal industry ___ ?",
+                {
+                    "devices": false,
+                    "clusters": true,
+                    "network": false,
+                    "things": false
+                }
+            ],
+            ["Q-->18 DDS stands for ?",
+                {
+                    "data define service": false,
+                    "data distribution service": true,
+                    "distribution data service": false,
+                    "none of these": false
+                }
+            ],
+            ["Q--> 19 M2M stands for ?",
+                {
+                    "machine to machine": true,
+                    "machine to home ": false,
+                    "many to many": false,
+                    "money to money": false
+                }
+            ],
+            ["Q--> 20 which symbol is used in arduino to calculate module?",
+                {
+                    "%": true,
+                    "* ": false,
+                    "%": false,
+                    "!": false
+                }
+            ],
+            ["Q-->21 which of the following is iot functional block under  logical design of IOT ?",
+                {
+                    "communication": false,
+                    "device": false,
+                    "service": false,
+                    "all of above": true
+                }
+            ],
+            ["Q-->22 which one is example of iot ?",
+                {
+                    "remote monitoring": false,
+                    "smart cities": true,
+                    "fleet control": false,
+                    "all of these": false
+                }
+            ],
+            ["Q-->23 __are the application of iot?",
+                {
+                    "house": false,
+                    "virtual environment": false,
+                    "regional office": false,
+                    "all of these": true
+                }
+            ],
+            ["Q-->24 __are the characteristics of iot ?",
+                {
+                    "intelligence": false,
+                    "scalable": false,
+                    "heteogeneity": false,
+                    "all of these": true
+                }
+            ],
+            ["Q-->25 The standard length of MAC address is?",
+                {
+                    "16": false,
+                    "48": true,
+                    "64": true,
+                    "8": false
+                }
+            ],
+            ["Q-->26 Which protocal is used to link all the devices in the Iot ?",
+                {
+                    "TCP/IP":true,
+                    "UTP": false,
+                    "HTTP": false,
+                    "NETWORK": false
+                }
+            ],
+            ["Q-->27 ___ allow us to control electronic components ?",
+                {
+                    "RETful API": false,
+                    "RESTful API": true,
+                    "HTTP": false,
+                    "MQTT": false
+                }
+            ],
+            ["Q-->28 What is the first IoT device that was the made by John Romkey??",
+                {
+                    "Toaste": true,
+                    "Sensor": false,
+                    "Mobile phone": false,
+                    "NONE": false
+                }
+            ],
+            ["Q-->29 When was the born of Internet of Things?",
+                {
+                    "1998": false,
+                    "2000": false,
+                    "2008": true,
+                    "2014": false
+                }
+            ],
+            ["Q-->30 what is sensor??",
+                {
+                    "input devices": true,
+                    "output devices": false,
+                    "both": false,
+                    "none": false
+                }
+            ],
+            ["Q-->31 Which of the following is not a main element of IoT?",
+                {
+                    "security": false,
+                    "people": true,
+                    "process": false,
+                    "things": false
+                }
+            ],
+            ["Q-->32 Who operates the core element?",
+                {
+                    "PaaS": false,
+                    "IaaS": false,
+                    "IoT service provider": true,
+                    "SaaS": false
+                }
+            ],
+            ["Q-->33 What bigdata collects ?",
+                {
+                    "Machine generated data": false,
+                    "Human generated data": true,
+                    "Store data": false,
+                    "relational data": false
+                }
+            ],
+            ["Q-->34 Through API,........... service portability is enabled?",
+                {
+                    "devices": false,
+                    "network": false,
+                    "system": true,
+                    "things": false
+                }
+            ],
+            ["Q-->35 IIoT stands for ....?",
+                {
+                    "Intense Internet of things": false,
+                    "Index Internet of technology": false,
+                    "Incorporate Internet of Technology": false,
+                    "Industrial Internet of Things": true
+                }
+            ],
+            ["Q-->36 which is not an IOT platform?",
+                {
+                    "Xiaomi cloud": false,
+                    "google cloud": false,
+                    "myntra": true,
+                    "AWS": false
+                }
+            ],["Q-->37 ___ is a modular and cloud based plateform?",
+                {
+                    "Eclipse Kapua": true,
+                    "Eclipse": false,
+                    "Eclipse Kura": false,
+                    "none": false
+                }
+            ],
+            ["Q-->38 IOT device should consume _____ energy?",
+                {
+                    "low": true,
+                    "high": false,
+                    "mid": false,
+                    "none": false
+                }
+            ],
+            ["Q-->39 ___ company first coined fog computing ?",
+                {
+                    "IBM": false,
+                    "cisco": true,
+                    "google": false,
+                    "microshoft": false
+                }
+            ],
+            ["Q-->40 MQTT  stands for?",
+                {
+                    "massage queuing text transport": false,
+                    "massage queue text transport": false,
+                    "massage queuing telemetry transport": true,
+                    "all of above": false
+                }
+            ],
+            ["Q-->41 SaaS stands for?",
+                {
+                    "service as a services": false,
+                    "platform as a service": false,
+                    "software as a service": true,
+                    "none of these": false
+                }
+            ],
+            ["Q-->42 CAN  stands for?",
+                {
+                    "compus area network": false,
+                    "corporate area network": false,
+                    "Both": true,
+                    "none": false
+                }
+            ],
+            ["Q-->43 How many layers in OSI?",
+                {
+                    "3": false,
+                    "4": false,
+                    "7": true,
+                    "8": false
+                }
+            ],
+            ["Q-->44 How many layers in TCP/IP?",
+                {
+                    "3": false,
+                    "4": true,
+                    "7": false,
+                    "8": false
+                }
+            ],
+            ["Q-->45 which address schemes are used by IPv4 and IPv6 internet protocols?",
+                {
+                    "128-bit and 32-bit": false,
+                    "32-bit and 128-bit": true,
+                    "32-bit and 64-bit": false,
+                    "16-bit 128-bit": false
+                }
+            ],
+            ["Q-->46 which one is not type of memory?",
+                {
+                    "NVRAM": false,
+                    "EPROM": false,
+                    "MASKED ROM": false,
+                    "DPROM": true
+                }
+            ],
+            ["Q-->47 The LPWAP stands for?",
+                {
+                    "low power wide area network": true,
+                    "low bit rate": false,
+                    "Both": false,
+                    "stort range wirelass commnunication": false
+                }
+            ],
+            ["Q-->48 full from of iot?",
+                {
+                    "internet of things":true,
+                    "information of things": false,
+                    "internet of technology": false,
+                    "None of these": false
+                }
+            ],
+            ["Q-->49 is heard of computer system?",
+                {
+                    "memory": false,
+                    "I/O": false,
+                    "processor": true,
+                    "counter": false
+                }
+            ],
+            ["Q-->50 end to end connection is established by protocols of the ICP/IP suite?",
+                {
+                    "IP": false,
+                    "UDP": false,
+                    "TCP": true,
+                    "All of these": false
+                }
+            ]
+
+        ];
+
+        let index = 0;
+        let total = 0;
+        let answered = new Array(q.length).fill(false);
+        let selectedAnswers = new Array(q.length).fill(null);
+        let que = document.getElementById("question");
+        let feedback = document.getElementById("feedback");
+        let h2 = document.createElement("h2");
+        que.appendChild(h2);
+
+        let buttons = [];
+        for (let i = 0; i < 4; i++) {
+            let btn = document.createElement("button");
+            btn.className = "b";
+            btn.addEventListener("click", () => ans(btn.textContent));
+            que.appendChild(btn);
+            buttons.push(btn);
+        }
+
+        function render() {
+            let current = q[index];
+            h2.textContent = current[0];
+            let options = current[1];
+            let keys = Object.keys(options);
+            for (let i = 0; i < 4; i++) {
+                buttons[i].textContent = keys[i];
+                buttons[i].disabled = answered[index];
+                buttons[i].style.backgroundColor = ""; 
+            }
+            feedback.textContent = "";
+        }
+
+        function next() {
+            index++;
+            if (index >= q.length) {
+                index = 0;
+            }
+            render();
+        }
+
+        function privious() {
+            index--;
+            if (index < 0) {
+                index = q.length - 1;
+            }
+            render();
+        }
+
+        function ans(selectedAns) {
+            if (answered[index]) return;
+            let options = q[index][1];
+            selectedAnswers[index] = selectedAns;
+            answered[index] = true;
+            if (options[selectedAns]) {
+                total++;
+            }
+            render();
+        }
+
+        function submitQuiz() {
+            total = 0; // Recalculate total
+            for (let i = 0; i < q.length; i++) {
+                let options = q[i][1];
+                let correctAnswer = Object.keys(options).find(key => options[key]);
+                if (selectedAnswers[i] === correctAnswer) {
+                    total++;
+                }
+            }
+            feedback.textContent = "Your total correct answers: " + total;
+            feedback.style.color = "blue";
+            document.getElementById("quiz").style.display="none";
+            document.getElementById("detail").style.display="block";
+
+            showCorrectAnswers();
+            information();
+        }
+
+        function showCorrectAnswers() {
+            let current = q[index];
+            let options = current[1];
+            let keys = Object.keys(options);
+            let correctAnswer = keys.find(key => options[key]);
+
+            for (let i = 0; i < 4; i++) {
+                let btn = buttons[i];
+                let btnText = btn.textContent;
+                btn.disabled = true; // Disable buttons after submission
+                
+            }
+        }
+
+        // Whenever you render, if already submitted, show correct answer
+        function render() {
+            let current = q[index];
+            h2.textContent = current[0];
+            let options = current[1];
+            let keys = Object.keys(options);
+            for (let i = 0; i < 4; i++) {
+                let btn = buttons[i];
+                btn.textContent = keys[i];
+                btn.disabled = answered[index];
+                btn.style.backgroundColor = "";
+            }
+            feedback.textContent = "";
+
+            // If quiz already submitted, show correct answer for this question
+            if (feedback.textContent !== "") {
+                showCorrectAnswers();
+            }
+        }
+function information(){
+    var roll_no=document.getElementById("roll").value;
+    var Name=document.getElementById("name").value;
+    var f_name=document.getElementById("f_name").value;
+    var course=document.getElementById("course").value;
+    document.getElementById("yourroll").innerHTML="noll NO -> "+roll_no;
+    document.getElementById("yourname").innerHTML="Name -> "+Name
+    document.getElementById("yourf_name").innerHTML="Father Name -> "+f_name;
+    document.getElementById("yourcourse").innerHTML="Roll NO -> "+course;
+            
+    }
+        function from_submit(){
+            var roll_no=document.getElementById("roll").value;
+            var Name=document.getElementById("name").value;
+            var f_name=document.getElementById("f_name").value;
+            var course=document.getElementById("course").value;
+            if(roll_no =="" || Name =="" || f_name =="" || course ==""){
+                alert("Please fill all info");
+            }
+            document.getElementById("quiz").style.display="block";
+            document.getElementById("form").style.display="none";
+        }
+        let t=document.getElementById("time");
+        let total_time=30*60;
+        let min=29;
+        let sec=60;
+        function show_time(){
+            if(sec==0){
+                t.innerHTML="Time out -->"+min+": "+sec;
+                sec=60;
+                min--;
+            }
+            t.innerHTML="Time out -->"+min+": "+sec;
+            sec--;
+
+        }
+    setInterval(show_time, 1000);
+    let time_up=60000*30;
+    setTimeout(function(){
+        alert("your exam finished");
+        t.style.display="none";
+        submitQuiz();
+        }, time_up)
+            render();
+            //information();
